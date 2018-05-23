fixed gradient computation

assignment1/spring1718_assignment1/cs231n/classifiers/linear_svm.py
@@ -46,7 +46,7 @@ def svm_loss_naive(W, X, y, reg):
 
   # Add regularization to the loss.
   loss += reg * np.sum(W * W)
-  dW = reg * W
+  dW += reg * W
 
   #############################################################################
   # TODO:                                                                     #