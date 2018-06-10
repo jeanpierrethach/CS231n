26f9f98bba4bd0218f7b4cb82ad0171da4e8703a
Thach Jean-Pierre <jeanpierre.thach@gmail.com>
2018-06-10 19:50:59 -0400

    added affine layernorm relu layers

--- assignment2/spring1718_assignment2_v2/cs231n/layer_utils.py
+++ assignment2/spring1718_assignment2_v2/cs231n/layer_utils.py
@@ -44,6 +44,21 @@ def affine_batchnorm_relu_backward(dout, cache):
     dx, dw, db = affine_backward(da1, fc_cache)
     return dx, dw, db, dgamma, dbeta
 
+def affine_layernorm_relu_forward(x, w, b, gamma, beta, ln_param):
+    a1, fc_cache = affine_forward(x, w, b)
+    a2, ln_cache = layernorm_forward(a1, gamma, beta, ln_param)
+    out, relu_cache = relu_forward(a2)
+    cache = (fc_cache, ln_cache, relu_cache)
+    return out, cache
+
+def affine_layernorm_relu_backward(dout, cache):
+    fc_cache, ln_cache, relu_cache = cache
+    da2 = relu_backward(dout, relu_cache)
+    da1, dgamma, dbeta = layernorm_backward(da2, ln_cache)
+    dx, dw, db = affine_backward(da1, fc_cache)
+    return dx, dw, db, dgamma, dbeta
+
+
 def conv_relu_forward(x, w, b, conv_param):
     """
     A convenience layer that performs a convolution followed by a ReLU.
